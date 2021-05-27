4a951959c2b17242ec929fe36b71e7478780ddf2
Stealth-git <rebill2me@gmail.com>
2021-05-27 21:43:23 +0300

    update locales

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module NewsBlog
     config.load_defaults 6.1
     
     config.time_zone = 'Moscow'
+    config.i18n.available_locales = [:ru, :en]
     config.i18n.default_locale = :ru
     
     # Configuration for the application, engines, and railties goes here.
